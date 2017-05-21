d33bf35b396c7cc64e1f9ef3eb431ae5bb5dad66
janeosaur <janewie@gmail.com>
2017-05-20 17:53:41 -0700

    new and post creature functions work

--- bog_app/config/routes.rb
+++ bog_app/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
   root "creatures#index"
-  # get "/", to: "creatures#index" ?
+  # same thing as get "/", to: "creatures#index" ?
 
   get "/creatures", to: "creatures#index", as: "creatures"
-
+  get "/creatures/new", to: "creatures#new", as: "new_creature"
+  post "/creatures", to: "creatures#create"
 end
