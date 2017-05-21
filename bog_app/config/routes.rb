cf9f4555a6df07471bfb308fbd9d76fd5d854e59
janeosaur <janewie@gmail.com>
2017-05-20 19:17:43 -0700

    create works

--- bog_app/config/routes.rb
+++ bog_app/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get "/", to: "creatures#index"
-  get "/creature", to: "creatures#index", as: "creatures"
+  get "/creatures", to: "creatures#index", as: "creatures"
+  get "/creatures/new", to: "creatures#new"
+  post "/creatures", to: "creatures#create"
 
 end
