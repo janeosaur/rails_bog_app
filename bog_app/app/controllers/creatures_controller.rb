5e971cdd7a877f2396826c4072838aac5419a25d
janeosaur <janewie@gmail.com>
2017-05-20 18:24:02 -0700

    update works

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -8,7 +8,6 @@ class CreaturesController < ApplicationController
   end
 
   def create
-    creature_params = params.require(:creature).permit(:name, :description)
     creature = Creature.new(creature_params)
 
     if creature.save
@@ -19,7 +18,30 @@ class CreaturesController < ApplicationController
 
   def show
     creature_id = params[:id]
-    @creature = Creature.find_by_id(creature_id)
+    # @creature = Creature.find_by_id(creature_id)
+    @creature = Creature.find creature_id
+  end
+
+  def edit
+    creature_id = params[:id]
+    @creature = Creature.find creature_id
+  end
+
+  def update
+    creature_id = params[:id]
+    creature = Creature.find creature_id # why are these necessary if edit has them?
+    creature.update_attributes(creature_params)
+    redirect_to creature_path(creature)
+    # equivalent to "/creatures/#{creature.id}" - is this bc of line 33?
+  end
+
+  private
+
+  def creature_params
+    params.require(:creature).permit(:name, :description)
   end
 
 end
+
+
+#why does update not use @creature but edit does?
--- bog_app/config/routes.rb
+++ bog_app/config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
   get "/creatures", to: "creatures#index", as: "creatures"
   get "/creatures/new", to: "creatures#new", as: "new_creature"
   post "/creatures", to: "creatures#create"
-  get "/creatures/:id", to:"creatures#show", as: "creature"
+  get "/creatures/:id", to: "creatures#show", as: "creature"
+  get "/creatures/:id/edit", to: "creatures#edit", as: "edit_creature"
+  patch "/creatures/:id", to: "creatures#update"#, as: "update_creature"
+  # how to choose which paths will have path helper names?
+
 end
