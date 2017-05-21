1c1e34f6d6c9baf3a0a65bedc4e390a236cc775b
janeosaur <janewie@gmail.com>
2017-05-20 20:26:31 -0700

    added entries in seed

--- bog_app/db/seeds.rb
+++ bog_app/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Creature.create({name: "a", description: "b"})
+Creature.create({name: "d", description: "e"})
