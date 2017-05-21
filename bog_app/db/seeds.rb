f028a57496fd72330132c78bf4459cdc83544628
janeosaur <janewie@gmail.com>
2017-05-20 21:10:55 -0700

    created controllers, model and seed

--- bog_app/db/seeds.rb
+++ bog_app/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Creature.destroy_all
+
+Creature.create({name: "Luke", description: "Jedi"})
+Creature.create({name: "Darth Vader", description: "Father of Luke"})
+Creature.create({name: "Yoda", description: "Little green man"})
+
+
+puts 'seed created'
